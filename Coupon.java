d11d6a0f5fc6f6049a1c1c30264645c19ec956a0
pri-2025 <prisha.parikh@cumminscollege.in>
2025-05-09 23:16:50 +0530

    Create Coupon.java

---
+++ Coupon.java
@@ -0,0 +1,10 @@
+package test;
+class Coupon {
+    String code;
+    double discountPercent;
+
+   public Coupon(String code, double discountPercent) {
+       this.code = code;
+       this.discountPercent = discountPercent;
+   }
+}
