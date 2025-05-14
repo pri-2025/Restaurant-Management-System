05dee37afe1b996dc80eaa2fbb0dbb0a3c58e0f3
pri-2025 <prisha.parikh@cumminscollege.in>
2025-05-14 09:40:31 +0530

    Update CardPayment.java

--- CardPayment.java
+++ CardPayment.java
@@ -1,13 +1,21 @@
 package test;
 
-class CardPayment extends Payment {
-    public CardPayment(double amount) {
-        super(amount);
-    }
+public class CardPayment extends Payment { // CardPayment class extends the abstract Payment class and provides
+											// implementation for card-based payment
+	public CardPayment(double amount) {
+		super(amount); // Constructor that passes the payment amount to the parent Payment class
+	}
 
-    public void processPayment(double discountPercent) {
-        double finalAmount = applyDiscount(discountPercent);
-        double taxamt= finalAmount + (0.18 * finalAmount);
-        System.out.println("Card payment of: " + taxamt + " after " + discountPercent + "% discount.");
-    }
+	@Override
+	public void processPayment(double discountPercent) {
+		double discountedAmount = applyDiscount(discountPercent); // Apply discount to the amount if applicable
+		double finalAmount = discountedAmount + (0.18 * discountedAmount); // Calculate final amount by adding 18% tax
+
+		// Display billing and payment details
+		System.out.println("Tax:18%");
+		System.out.println("Card payment of: ₹" + finalAmount);
+		System.out.println("Please insert the card");
+		System.out.println("Your transaction is being processed");
+		System.out.println("Please collect the receipt..");
+	}
 }
