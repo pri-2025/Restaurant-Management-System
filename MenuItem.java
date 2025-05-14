ca2f9410cf5dbf7c40ebd4b81011d77b1b27e744
pri-2025 <prisha.parikh@cumminscollege.in>
2025-05-14 09:34:00 +0530

    Update MenuItem.java

--- MenuItem.java
+++ MenuItem.java
@@ -1,17 +1,24 @@
 package test;
-import java.util.*;
-public class MenuItem {
-    int number;
-    String name;
-    double price;
 
-    public MenuItem(int number, String name, double price) {
-        this.number = number;
-        this.name = name;
-        this.price = price;
-    }
-    public String toString() {
-        return number + ". " + name + " - ₹" + price;
-    }
+public class MenuItem { // Class representing a single item in the menu
+	int number; // Item number shown in the menu
+	String name; // Name of the menu item
+	double price; // Price of the item
+
+	// Constructor to initialize all fields
+	public MenuItem(int number, String name, double price) {
+		this.number = number;
+		this.name = name;
+		this.price = price;
+	}
+
+	// Default constructor (used when no initial values are provided)
+	public MenuItem() {
+		// Default constructor
+	}
+
+	// String representation of the MenuItem, used when printing the item
+	public String toString() {
+		return number + ". " + name + " - ₹" + price;
+	}
 }
-
