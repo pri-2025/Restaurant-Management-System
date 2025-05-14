6f37bdb50bbbd6fe25cd574267773dd235d905f9
pri-2025 <prisha.parikh@cumminscollege.in>
2025-05-14 09:41:06 +0530

    Update OnlinePayment.java

--- OnlinePayment.java
+++ OnlinePayment.java
@@ -1,14 +1,20 @@
 package test;
 
-class OnlinePayment extends Payment {
-    public OnlinePayment(double amount) {
-        super(amount);
-    }
+public class OnlinePayment extends Payment { // OnlinePayment class extends the abstract Payment class and provides
+												// implementation for online-based payment
+	public OnlinePayment(double amount) {
+		super(amount); // Constructor that passes the payment amount to the parent Payment class
+	}
 
-    public void processPayment(double discountPercent) {
-        double finalAmount = applyDiscount(discountPercent);
-        double taxamt= finalAmount + (0.18 * finalAmount);
-        System.out.println("Online payment of: " + taxamt + " after " + discountPercent + "% discount.");
-       
-    }
+	@Override
+	public void processPayment(double discountPercent) {
+		double discountedAmount = applyDiscount(discountPercent); // Apply discount to the amount if applicable
+		double finalAmount = discountedAmount + (0.18 * discountedAmount); // Calculate final amount by adding 18% tax
+
+		// Display billing and payment details for online payment
+		System.out.println("Tax:18%");
+		System.out.println("Online payment of: ₹" + finalAmount);
+		System.out.println("Please scan the QR code to pay");
+		System.out.println("Please collect the receipt.");
+	}
 }
