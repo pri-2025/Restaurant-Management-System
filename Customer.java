d87257558a50d2ca8b421ea44135a026477910d5
pri-2025 <prisha.parikh@cumminscollege.in>
2025-05-14 09:42:27 +0530

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -3,26 +3,46 @@ package test;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Customer {
-    String name;
-    int table_num;
+public class Customer { // Customer class represents a customer with a name and discount percentage
 
-    public Customer(String name) {
-        this.name = name;
-    }
+	// Instance variable to store the customer's name
+	String name;
 
-    public void assignTable(ArrayList<Integer> assignedTables) {
-        Random rand = new Random();
-        int newTable;
-        do {
-            newTable = 1 + rand.nextInt(15);  // Table numbers from 1 to 15
-        } while (assignedTables.contains(newTable));
-        this.table_num = newTable;
-        assignedTables.add(newTable);
-        System.out.println("Hello " + name + "! You are assigned table number: " + table_num);
-    }
+	// Instance variable to store the discount percentage for the customer
+	double discountPercent;
 
-    public int getTableNum() {
-        return table_num;
-    }
+	// Constructor to initialize the customer with a name and no discount initially
+	public Customer(String name) {
+		this.name = name;
+		this.discountPercent = 0.0; // No discount by default
+	}
+
+	// Method to randomly assign a coupon to the customer
+	public void assignCoupon() {
+		Random rand = new Random(); // Creating an instance of Random to generate random values
+		System.out.println("Random Coupon Generator");
+
+		int chance = rand.nextInt(100); // generates a number between 0 and 99
+
+		if (chance < 50) { // 50% chance to receive a coupon
+			// Random discount between 10% to 20%
+			this.discountPercent = 10 + rand.nextInt(11);
+			// Displaying the discount message
+			System.out.println(
+					"Congratulations " + name + "! You've received a " + discountPercent + "% discount coupon!");
+		} else {
+			// If no coupon is given, print a message
+			System.out.println("Hi " + name + ", no coupon this time. Better luck next time!");
+		}
+	}
+
+	// Getter for the customer name
+	public String getName() {
+		return name;
+	}
+
+	// Getter for the coupon value (discount percentage)
+	public double getDiscountPercent() {
+		return discountPercent;
+	}
 }
