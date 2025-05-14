4ae1b03b6524b5b5b7e2cdb420586d556f222306
pri-2025 <prisha.parikh@cumminscollege.in>
2025-05-14 09:37:19 +0530

    Update Payment.java

--- Payment.java
+++ Payment.java
@@ -1,23 +1,27 @@
 package test;
-public class OrderItem {
-     MenuItem item;
-     int quantity;
-    public OrderItem(){
-       MenuItem item;
-        int quantity;
-    }
 
-    public OrderItem(MenuItem item, int quantity) {
-        this.item = item;
-        this.quantity = quantity;
-    }
-   
+public abstract class Payment { // Abstract class Payment represents the common structure for all payment types
+	double amount; // Instance variable to store the payment amount
 
-    public  double getTotalPrice() {
-        return item.price * quantity;
-    }
+	// Constructor to initialize the amount for the payment
+	public Payment(double amount) {
+		this.amount = amount;
+	}
 
-    public String toString() {
-        return item.name + " x " + quantity + " = ₹" + getTotalPrice();
-    }
+	// Getter method to retrieve the payment amount
+	public double getAmount() {
+		return amount;
+	}
+
+	// Method to apply the discount on the given amount
+	// It calculates the new amount after applying the discount
+	public double applyDiscount(double discountPercent) {
+		double a = amount - (amount * discountPercent / 100);
+		System.out.println("Amount after discount: ₹" + a);
+		return a;
+	}
+
+	// Abstract method to process the payment based on the discount percentage
+	// This method must be implemented by subclasses
+	public abstract void processPayment(double discountPercent);
 }
